273989b74ab8b245171d5c995e253e4eaccfc69e
Sumit Yadav <sumityadav6541@gmail.com>
2016-06-30 13:09:23 +0530

    Fixed the sampleApps2 Project removed the unnecessary dependencies.

--- sampleApp2/src/main/java/com/spring/test/HelloSpringApp.java
+++ sampleApp2/src/main/java/com/spring/test/HelloSpringApp.java
@@ -1,16 +1,31 @@
 package com.spring.test;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloSpringApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		
+		/*Just to debug the project*/
+		/*PrintWriter writer = new PrintWriter("test.txt", "UTF-8");
+		writer.println("The first line");
+		writer.println("The second line");
+		writer.println("The third line");
+		writer.close();*/
+		
+		
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		/*ICoach theCoach = context.getBean("myCoach", ICoach.class);
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
 		context.close();*/
+		
+		
 		ICoach theCoach = context.getBean("baseballCoach", ICoach.class);
 		ICoach theCoach2 = context.getBean("baseballCoach", ICoach.class);
 		System.out.println(theCoach.getDailyWorkout());
